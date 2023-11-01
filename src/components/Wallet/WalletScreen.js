5a16f29a11794e7c71c89df52a851ee326bf3ad5
Jared5610 <jaredfligor@gmail.com>
2023-11-01 14:13:40 -0400

    Updated Wallet Screen

--- src/components/Wallet/WalletScreen.js
+++ src/components/Wallet/WalletScreen.js
@@ -7,9 +7,7 @@ const WalletScreen = () => {
   const [newCard, setNewCard] = useState({ cardNumber: '', cardType: '' });
   const [isViewingCards, setIsViewingCards] = useState(false);
 
-  // Function to add a new credit card
   const addCreditCard = () => {
-    // Validate card details here 
     if (newCard.cardNumber && newCard.cardType) {
       setCreditCards([...creditCards, newCard]);
       setIsAddingCard(false);
@@ -18,31 +16,31 @@ const WalletScreen = () => {
       alert('Please enter valid card details.');
     }
   };
+  
+  const removeCreditCard = (cardIndex) => {
+    const updatedCards = creditCards.filter((_, index) => index !== cardIndex);
+    setCreditCards(updatedCards);
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.header}>My Wallet</Text>
-      <View style={styles.buttonContainer}>
+      <View style={styles.buttonGroup}>
         <Button title="Add Credit Card" onPress={() => setIsAddingCard(true)} />
-      </View>
-      <View style={styles.buttonContainer}>
         <Button title="View All Cards" onPress={() => setIsViewingCards(true)} />
       </View>
 
-      {/* List of credit cards */}
       <FlatList
         data={creditCards}
         keyExtractor={(item, index) => index.toString()}
         renderItem={({ item }) => (
           <View style={styles.cardContainer}>
             <Text style={styles.cardText}>Card Type: {item.cardType}</Text>
-            <Text style={styles.cardText}>Card Number: {item.cardNumber}</Text>
-            {/* Add edit and delete options here */}
+            <Text style={styles.cardText}>Card Number: **** **** **** {item.cardNumber.slice(-4)}</Text>
           </View>
         )}
       />
 
-      {/* Modal for adding a new credit card */}
       <Modal visible={isAddingCard}>
         <View style={styles.modalContainer}>
           <Text style={styles.header}>Add Credit Card</Text>
@@ -58,17 +56,27 @@ const WalletScreen = () => {
             value={newCard.cardType}
             onChangeText={(text) => setNewCard({ ...newCard, cardType: text })}
           />
-          <View style={styles.buttonContainer}>
+          <View style={styles.buttonGroup}>
             <Button title="Add" onPress={addCreditCard} />
             <Button title="Cancel" onPress={() => setIsAddingCard(false)} />
           </View>
         </View>
       </Modal>
 
-      {/* Modal for viewing all credit cards */}
       <Modal visible={isViewingCards}>
         <View style={styles.modalContainer}>
           <Text style={styles.header}>All Credit Cards</Text>
+          <FlatList
+            data={creditCards}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item, index }) => (
+              <View style={styles.cardContainer}>
+                <Text style={styles.cardText}>Card Type: {item.cardType}</Text>
+                <Text style={styles.cardText}>Card Number: **** **** **** {item.cardNumber.slice(-4)}</Text>
+                <Button title="Remove" onPress={() => removeCreditCard(index)} />
+              </View>
+            )}
+          />
           <Button title="Close" onPress={() => setIsViewingCards(false)} />
         </View>
       </Modal>
@@ -79,41 +87,63 @@ const WalletScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
+    padding: 20,
+    backgroundColor: '#ecf0f1',
+  },
+  header: {
+    fontSize: 28,
+    marginBottom: 30,
+    alignSelf: 'center',
+    color: '#2c3e50',
+    fontWeight: 'bold',
+  },
+  buttonGroup: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 30,
+    alignItems: 'center',
+  },
+  cardContainer: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 20,
+    marginBottom: 20,
+    elevation: 3,
+    shadowOffset: { width: 0, height: 1 },
+    shadowRadius: 3,
+    shadowOpacity: 0.2,
+  },
+  cardText: {
+    fontSize: 18,
+    marginBottom: 12,
+    color: '#34495e',
+  },
+  modalContainer: {
+    flex: 1,
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  header: {
-    fontSize: 24,
-    marginBottom: 16,
-  },
-  cardContainer: {
-    backgroundColor: '#f5f5f5',
-    borderRadius: 8,
-    padding: 16,
-    marginVertical: 8,
-  },
-  cardText: {
+  input: {
+    height: 45,
+    width: '85%',
+    borderWidth: 1,
+    borderColor: '#bdc3c7',
+    borderRadius: 10,
+    paddingHorizontal: 12,
+    marginBottom: 20,
     fontSize: 16,
   },
-  modalContainer: {
-    backgroundColor: 'white',
-    padding: 16,
-    borderRadius: 8,
-  },
-  input: {
-    height: 40,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 4,
-    paddingHorizontal: 8,
-    marginBottom: 16,
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    marginTop: 16,
-  },
+  buttonGroup: {
+    height: 82,  // adjust this value to your preference
+    marginTop: 20,
+    marginBottom: 30,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+},
+
 });
 
 export default WalletScreen;
